ec4831eec9ee34faffaf6f01c1243705aebf0a95
Dennis Loh <dennis@Denniss-MacBook-Air.local>
2021-05-30 22:32:23 +0800

    build: initial commit

---
+++ src/screens/project-list/index.tsx
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react";
+import { List } from "./list";
+import { SearchPanel } from "./search-panel";
+import * as qs from "qs";
+import { cleanObject, useDebounce, useMount } from "utils";
+
+const apiUrl = process.env.REACT_APP_API_URL;
+console.log(apiUrl);
+export const ProjectListScreen = () => {
+  const [param, setParam] = useState({
+    name: "",
+    personId: "",
+  });
+
+  const debouncedParam = useDebounce(param, 200);
+
+  const [users, setUsers] = useState([]);
+
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      `${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`
+    ).then(async (response) => {
+      if (response.ok) {
+        setList(await response.json());
+      }
+    });
+  }, [debouncedParam]);
+
+  useMount(() => {
+    fetch(`${apiUrl}/users`).then(async (response) => {
+      if (response.ok) {
+        setUsers(await response.json());
+      }
+    });
+  });
+
+  return (
+    <div>
+      <SearchPanel users={users} param={param} setParam={setParam} />
+      <List users={users} list={list} />
+    </div>
+  );
+};
