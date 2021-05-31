f40d95b0b837230d2ebd3f136dc258f30f3671e6
Dennis Loh <dennis@Denniss-MacBook-Air.local>
2021-06-01 00:50:19 +0800

    feat: handle fullpage loading and error

--- src/context/auth-context.tsx
+++ src/context/auth-context.tsx
@@ -3,6 +3,9 @@ import * as auth from "auth-provider";
 import { User } from "screens/project-list/search-panel";
 import { http } from "utils/http";
 import { useMount } from "utils";
+import { useAsync } from "utils/use-async";
+import { idText } from "typescript";
+import { FullPageErrorFallback, FullPageLoading } from "components/lib";
 
 interface AuthForm {
   username: string;
@@ -33,13 +36,30 @@ const AuthContext =
 AuthContext.displayName = `AuthContext`;
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
-  const [user, setUser] = useState<User | null>(null);
+  const {
+    data: user,
+    error,
+    isLoading,
+    isIdle,
+    isError,
+    run,
+    setData: setUser,
+  } = useAsync<User | null>();
+  // const [user, setUser] = useState<User | null>(null);
 
   const login = (form: AuthForm) => auth.login(form).then(setUser);
   const register = (form: AuthForm) => auth.register(form).then(setUser);
   const logout = () => auth.logout().then(() => setUser(null));
 
-  useMount(() => bootstrapUser().then(setUser));
+  useMount(() => run(bootstrapUser()));
+
+  if (isIdle || isLoading) {
+    return <FullPageLoading />;
+  }
+
+  if (isError) {
+    return <FullPageErrorFallback error={error} />;
+  }
 
   return (
     <AuthContext.Provider
