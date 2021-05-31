be61f9eceec48d7ae71ec0a88d6f488f51870756
Dennis Loh <dennis@Denniss-MacBook-Air.local>
2021-05-31 09:39:56 +0800

    feat: add login status

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -3,6 +3,7 @@ import { List } from "./list";
 import { SearchPanel } from "./search-panel";
 import * as qs from "qs";
 import { cleanObject, useDebounce, useMount } from "utils";
+import { useHttp } from "utils/http";
 
 const apiUrl = process.env.REACT_APP_API_URL;
 
@@ -18,22 +19,14 @@ export const ProjectListScreen = () => {
 
   const [list, setList] = useState([]);
 
+  const client = useHttp();
+
   useEffect(() => {
-    fetch(
-      `${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`
-    ).then(async (response) => {
-      if (response.ok) {
-        setList(await response.json());
-      }
-    });
+    client(`projects`, { data: cleanObject(debouncedParam) }).then(setList);
   }, [debouncedParam]);
 
   useMount(() => {
-    fetch(`${apiUrl}/users`).then(async (response) => {
-      if (response.ok) {
-        setUsers(await response.json());
-      }
-    });
+    client(`users`).then(setUsers);
   });
 
   return (
